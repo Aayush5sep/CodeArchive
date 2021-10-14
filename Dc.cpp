69dfdcac8f50c0e849f8c69bfa17f8741cbaabe1
Aayush <92013588+Aayush5sep@users.noreply.github.com>
2021-10-14 15:20:39 +0530

    Create Dc.cpp

---
+++ Dc.cpp
@@ -0,0 +1,73 @@
+import os
+my_secret = os.environ['Token']
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot,has_permissions
+from webserver import keep_alive
+from replit import db
+import datetime
+
+bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+
+current = datetime.datetime.now()
+db['all_contests'] = []
+
+@bot.event
+async def on_ready():
+  print('Bot is ready')
+
+@bot.command()
+async def info(ctx):
+  await ctx.channel.send(f'Your Name :{ctx.author.name}')
+
+@bot.command()
+async def set(ctx, platform, cpid):
+  await ctx.channel.send(f'{platform} profile for {ctx.author.name} has been set to {cpid} ')
+  
+@bot.command(pass_context=True)
+@has_permissions(administrator=True)
+async def add(ctx,platform,day,month,hour,minutes,link,*,contest):
+	if platform.upper()=='CODEFORCES' or platform.upper()=='CODECHEF' or platform.upper()=='ATCODER':
+		db[contest]={}
+		temp=db[contest]
+		temp['platform']=platform.upper()
+		temp['contest']=contest
+		temp['day']=day
+		temp['month']=month
+		temp['hour']=hour
+		temp['minutes']=minutes
+		temp['link']=link
+		db[contest]=temp
+		db['all_contests'].append(contest)
+		await ctx.channel.send(f'Contest added: \n {contest} on {platform} wil be held on {day}/{month} at time {hour}/{minutes} \n Link: {link}')
+
+
+@bot.command(pass_context=True)
+@has_permissions(administrator=True)
+async def clear_contests(ctx):
+	for contests in db['all_contests']:
+		contest = contests
+		db['all_contests'].pop()
+		del db[contests]
+		await ctx.channel.send(f' {contest} \n Contest has been deleted by the owner')
+	
+	
+
+@bot.command()
+async def contests(ctx):
+  print('start executed')
+  for contests in db['all_contests']:
+  	print(contests)
+  	contests_list = discord.Embed(title="Contest",color=0x2ecc71)
+  	contests_list.add_field(name="Platform",value=db[contests]['platform'])
+  	contests_list.add_field(name="Contest Name",value=db[contests]['contest'])
+  	contests_list.add_field(name="Link",value=db[contests]['link'])
+  	await ctx.channel.send(embed=contests_list)
+  	print('for loop executed')
+  print('end executed')
+
+
+keep_alive()
+
+bot.run(my_secret)
